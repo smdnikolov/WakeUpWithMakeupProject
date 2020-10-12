5cda135acea135124d27869ceebff776707d9dbe
smdnikolov <smdnikolov@gmail.com>
2020-10-12 20:24:03 +0300

    Minor css fixes
    minor fixes

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -10,7 +10,6 @@ const Header = () => {
                 <img className="logo" src={logo} width="90%" alt="" />
             </Link>
             <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-                <a class="navbar-brand" href="#">Navbar</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                     <span class="navbar-toggler-icon"></span>
                 </button>
