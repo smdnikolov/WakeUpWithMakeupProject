10def6404dabadc96124cfbc9eb42660748aaf6c
smdnikolov <smdnikolov@gmail.com>
2020-12-05 17:52:27 +0200

    Contacts
    Edited the contaacts component, as well as replacing some styles on the scroll to top component

--- src/components/contacts/index.js
+++ src/components/contacts/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
-
+import map from '../../utils/map.jpg'
 
 const Contacts = () => {
 
     return (
         <div>
-            <h1>Contacts</h1>
+            <h1 className='page-header'>Контакти</h1>
+
+            <h3>Телефон: <b>+359879997600</b></h3>
+            <hr />
+            <h3>Електронна поща: <b>elenayordanova0440@gmail.com</b></h3>
+            <hr />
+            <h3>Работно време: <b> Всеки ден от 9:00 до 19:00</b></h3>
+            <hr />
+            <h3>Адрес на салона:</h3>
+            <a target='_blank' rel="noopener noreferrer" href='https://goo.gl/maps/vxEXZHnkJwNuQQaF7'>
+                <img className='img map' src={map} alt='' />
+            </a>
         </div>
+
     )
 }
 
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -16,13 +16,6 @@ const Home = () => {
                 <img className='img' slot="before" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/before.jpg" alt='' />
                 <img className='img' slot="after" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/after.jpg" alt='' />
             </img-comparison-slider>
-            {/* <div id="slider1" data-beer-label="before">
-                <img className='img' src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/before.jpg" alt="" />
-                <div className="beer-reveal" data-beer-label="after">
-                    <img className='img' src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/after.jpg" alt="" />
-                </div>
-            </div> */}
-
         </div>
 
     )
