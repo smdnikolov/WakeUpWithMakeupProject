f71f39b0158e403b037e13b933a917f788e7a11e
smdnikolov <smdnikolov@gmail.com>
2020-12-06 18:53:53 +0200

    Prices componenet
    Added styles for the component

--- src/components/prices/index.js
+++ src/components/prices/index.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import priceList from '../../price-list'
+import './styles.scss'
 
 const Prices = () => {
-    const asd = [...priceList]
-    const Test = ({ priceList }) => (
-        <div>
-            {priceList.map((obj, index) => (
-                <div key={obj.service}>
-                    <div>
-                        {`${obj.service} - ${obj.price} with index ${index}`}
-                    </div>
-                </div>
-            ))}
+    const list = [...priceList]
+    const Table = ({ priceList }) => (
+        <div className="container">
+            <ul className="responsive-table">
+                <li className="table-header">
+                    <div className="col col-1">Услуга</div>
+                    <div className="col col-4">Цена</div>
+                </li>
+                {priceList.map(obj => (
+                    <li className='table-row' id={obj.id} key={obj.id}>
+                        <div className="col col-1 col-2 col-3" data-label="Услуга">{obj.service}</div>
+                        <div className="col col-4" data-label="Цена">{obj.price}</div>
+                    </li>
+                ))}
+
+
+            </ul>
         </div>
     );
     return (
         <div>
             <h1>Prices</h1>
             <div className='container'>
-                <Test priceList={asd} />
+                <Table priceList={list} />
             </div>
         </div>
     )
--- src/price-list.js
+++ src/price-list.js
@@ -1,27 +1,35 @@
 export default [
     {
+        id: 'price1',
         service: 'some service name',
         price: 'X BGN'
     },
     {
+        id: 'price2',
         service: 'some service name 2',
         price: 'X BGN'
     }, {
+        id: 'price3',
         service: 'some service name 3',
         price: 'X BGN'
     }, {
+        id: 'price4',
         service: 'some service name 4',
         price: 'X BGN'
     }, {
+        id: 'price5',
         service: 'some service name 5',
         price: 'X BGN'
     }, {
+        id: 'price6',
         service: 'some service name 6',
         price: 'X BGN'
     }, {
+        id: 'price7',
         service: 'some service name 7',
         price: 'X BGN'
     }, {
+        id: 'price8',
         service: 'some service name 8',
         price: 'X BGN'
     },
