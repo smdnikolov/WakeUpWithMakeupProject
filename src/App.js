23153ef4bfa3b570dbfb8fbca25a636c3e662e43
smdnikolov <smdnikolov@gmail.com>
2020-12-08 20:15:32 +0200

    Added some conditional rendering to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loader from './components/loader'
 import './App.css';
 import Router from './router/Router'
 import Header from './components/header'
@@ -6,14 +7,24 @@ import Footer from './components/footer'
 import ScrollToTop from './components/scroll-to-top'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
 
+
+    window.addEventListener('load', function () {
+      setLoading(false)
+    })
+
+
+  }, [loading])
   return (
-    <div className="App">
-      <Header />
-      <ScrollToTop />
-      <Router />
-      <Footer />
-    </div>
+    loading ? <Loader></Loader>
+      : <div className="App">
+        <Header />
+        <ScrollToTop />
+        <Router />
+        <Footer />
+      </div>
   )
 }
 
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import before from '../../utils/before.jpg'
+import after from '../../utils/after.jpg'
 import { defineCustomElements, applyPolyfills, } from 'img-comparison-slider/loader';
 
 applyPolyfills().then(() => defineCustomElements(window));
@@ -8,8 +10,8 @@ const Home = () => {
     return (
         <div >
             <img-comparison-slider>
-                <img className='img' slot="before" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/before.jpg" alt='' />
-                <img className='img' slot="after" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/74321/after.jpg" alt='' />
+                <img className='img' slot="before" src={before} alt='' />
+                <img className='img' slot="after" src={after} alt='' />
             </img-comparison-slider>
         </div>
     )
