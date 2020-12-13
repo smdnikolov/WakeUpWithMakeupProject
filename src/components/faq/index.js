d5f3ea0027edbe6bd0ed648e1a026e0e988e7b04
smdnikolov <smdnikolov@gmail.com>
2020-12-13 14:14:36 +0200

    Edited FAQ component

--- src/components/faq/index.js
+++ src/components/faq/index.js
@@ -7,7 +7,24 @@ const FAQ = () => {
 
     return (
         <div>
-            <h1>FAQ</h1>
+            <h1 className='page-header'>Често Здавани Въпроси</h1>
+
+            <div id="accordion" className='container'>
+                <div className="card">
+                    <div className="card-header" id="headingOne">
+                        <h3 className="mb-0">
+                            <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Collapsible Group Item #1
+                            </button>
+                        </h3>
+                    </div>
+                    <div id="collapseOne" className="collapse " aria-labelledby="headingOne" data-parent="#accordion">
+                        <div className="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
