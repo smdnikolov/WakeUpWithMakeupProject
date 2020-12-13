f4eaa18fe46ba210b8653299ba0a9bc5b796d00d
smdnikolov <smdnikolov@gmail.com>
2020-12-13 13:53:48 +0200

    Edited Gallery componenet
    Fix some css and functionality of the responsiveness of the componenet

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -1,33 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './styles.scss'
-import img1 from '../../utils/Erthea BW1.jpg'
-import img2 from '../../utils/Erthea- Brilliant-White-1.jpg'
 
+function importAll(r) {
+    return r.keys().map(r);
+}
+
+const images = importAll(require.context('../../utils/gallery', false, /\.(png|jpe?g|svg)$/));
+
+const GalleryList = () => {
+    return <div className='row gallery'>
+        {images.map((image, index) => {
+
+            if (window.screen.width < window.screen.height) {
+                // eslint-disable-next-line
+                return <a className="col-sm-4" key={`key-${index}`}>
+                    <img src={image} className="img-fluid" alt='' />
+                </a>
+            } else {
+                return <a href={image} data-toggle="lightbox" data-gallery="example-gallery" className="col-sm-4" key={`key-${index}`}>
+                    <img src={image} className="img-fluid" alt='' />
+                </a>
+            }
+
+        })}
+    </div>
+}
 const Gallery = () => {
 
+    const [, setDimensions] = useState({
+        height: window.innerHeight,
+        width: window.innerWidth
+    })
+    useEffect(() => {
+        window.addEventListener('resize', () => setDimensions({ height: window.innerHeight, width: window.innerWidth }))
+    })
+
     return (
-        <div >
+        <div>
             <h1 className="page-header">Галерия</h1>
             <div className="row justify-content-center">
                 <div className="col-md-8">
-                    <div className="row gallery">
-                        <a href={img1} data-toggle="lightbox" data-gallery="example-gallery" className="col-sm-4">
-                            <img src={img2} className="img-fluid" alt='' />
-                        </a>
-                        <a href={img1} data-toggle="lightbox" data-gallery="example-gallery" className="col-sm-4">
-                            <img src={img2} className="img-fluid" alt='' />
-                        </a>
-                        <a href={img1} data-toggle="lightbox" data-gallery="example-gallery" className="col-sm-4">
-                            <img src={img2} className="img-fluid" alt='' />
-                        </a>
-                        <a href={img1} data-toggle="lightbox" data-gallery="example-gallery" className="col-sm-4">
-                            <img src={img2} className="img-fluid" alt='' />
-                        </a>
-                        <a href={img1} data-toggle="lightbox" data-gallery="example-gallery" className="col-sm-4">
-                            <img src={img2} className="img-fluid" alt='' />
-                        </a>
-
-                    </div>
+                    <GalleryList />
                 </div>
             </div>
         </div>
