2926ba2584e76f71dbc452f830d72b7fa560d623
smdnikolov <smdnikolov@gmail.com>
2021-01-25 23:24:52 +0200

    Formated css

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -6,11 +6,10 @@ import "react-lightbox-component/build/css/index.css";
 function importAll(r) {
     return r.keys().map(r);
 }
-
 const images = importAll(require.context('../../utils/gallery', false, /\.(png|jpe?g|svg)$/));
-const a = []
+const list = []
 images.forEach((image) => {
-    a.push({ src: image.default, title: 'title', description: 'desc' })
+    list.push({ src: image.default, title: 'title', description: 'desc' })
 })
 
 const Gallery = () => {
@@ -19,13 +18,10 @@ const Gallery = () => {
             <h1 className="page-header">Галерия</h1>
             <div className="row justify-content-center container">
                 <div className="col-md-12">
-
                     <Lightbox
                         thumbnailWidth='300px'
                         thumbnailHeight='400px'
-                        // showImageModifiers={false}
-                        images={a} />
-
+                        images={list} />
                 </div>
             </div>
         </div>
