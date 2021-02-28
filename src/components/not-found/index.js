3af643b7b2e41ec80b6cdfbfe038143a364655eb
smdnikolov <smdnikolov@gmail.com>
2021-02-28 18:15:08 +0200

    Edited service component

--- src/components/not-found/index.js
+++ src/components/not-found/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div className="not-found">
-      <h1>Страницата не е намерена</h1>
-      <img src={'/404.png'} alt="" />
+    <div className='not-found'>
+      <h1 className='page-header'>Страницата не е намерена</h1>
+      <img src={'/404.png'} alt='' />
     </div>
   );
 };
--- src/components/service/index.js
+++ src/components/service/index.js
@@ -3,26 +3,27 @@ import services from '../services/services';
 import NotFound from '../not-found';
 
 const Service = (props) => {
-  let serviceTitle = props.location.pathname.split('/')[
-    props.location.pathname.split('/').length - 1
-  ];
+  let serviceTitle = props.location.pathname.split('/')[props.location.pathname.split('/').length - 1];
   let titles = [];
   services.forEach((x) => {
     titles.push(x.title.toLocaleLowerCase().replaceAll(' ', '-'));
   });
-  const service = services.filter(
-    (x) => x.title.toLocaleLowerCase().replaceAll(' ', '-') === serviceTitle
-  )[0];
+  const service = services.filter((x) => x.title.toLocaleLowerCase().replaceAll(' ', '-') === serviceTitle)[0];
+
+  function NewlineText(props) {
+    const text = props.text;
+    return text.split('\n').map((str, index) => <p key={index}>{str}</p>);
+  }
 
   if (!titles.includes(serviceTitle)) {
     return <NotFound />;
   } else {
     return (
       <>
-        <div className="container">
-          <h1 className="page-header">{service.title}</h1>
+        <div className='container'>
+          <h1 className='page-header'>{service.title}</h1>
           <div
-            className="heading"
+            className='heading'
             style={{
               background: `url(${service.image}) no-repeat center`,
               marginBottom: '50px',
@@ -30,47 +31,103 @@ const Service = (props) => {
               MozBbackgroundSize: 'cover',
               OBackgroundSize: 'cover',
               backgroundSize: 'cover',
-            }}
-          ></div>
-          <div className="row">
-            <div className="col">
-              <p
-                style={{
-                  textAlign: 'justify',
-                }}
-              >
-                {service.description}
-              </p>
-              <div className="service-row">
-                <div className="underline" />
-                <h3>Ползи</h3>
-                <div className="underline" />
-                <p
-                  style={{
-                    textAlign: 'center',
-                  }}
-                >
-                  {service.benefits}
-                </p>
-                <div className="underline" />
-                <h3>Етапи</h3>
-                <div className="underline" />
-                <ul className="stages">
-                  {service.stages.map((x, index) => {
-                    return (
-                      <li className="stage" key={index}>
-                        <img
-                          src={x[Object.keys(x)[0]]}
-                          style={{ width: '50px' }}
-                          alt=""
-                        />
-                        <p>
-                          {index + 1}. {Object.keys(x)[0]}
-                        </p>
-                      </li>
-                    );
-                  })}
-                </ul>
+            }}></div>
+          <div className='row'>
+            <div className='col'>
+              <div className='text-content'>{service.description}</div>
+              <div className='service-row'>
+                {service.benefits ? (
+                  <>
+                    <div className='underline' />
+                    <h3>Ползи</h3>
+                    <div className='underline' />
+                    <div className='text-content'>{service.benefits}</div>
+                  </>
+                ) : null}
+
+                {service.stages ? (
+                  <>
+                    <div className='underline' />
+                    <h3>Етапи</h3>
+                    <div className='underline' />
+                    <ul className='stages'>
+                      {service.stages.map((x, index) => {
+                        return (
+                          <li className='stage' key={index}>
+                            <img src={x[Object.keys(x)[0]]} style={{ width: '50px' }} alt='' />
+                            <p>
+                              {index + 1}. {Object.keys(x)[0]}
+                            </p>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </>
+                ) : null}
+
+                {service.preparation ? (
+                  <>
+                    <div className='underline'></div>
+                    <h3>Подготовка за процедурата</h3>
+                    <div className='underline'></div>
+                    <div className='text-content'>
+                      <NewlineText text={service.preparation} />
+                    </div>
+                  </>
+                ) : null}
+
+                {service.sideEffectsAbsolute && service.sideEffectsMinor ? (
+                  <>
+                    <div className='underline'></div>
+                    <h3> Противопоказания</h3>
+                    <div className='underline'></div>
+                    <div className='row'>
+                      <div className='col'>
+                        <h2 style={{ textDecoration: 'underline' }}>Абсолютни:</h2>
+                        <div className='text-content'>
+                          <NewlineText text={service.sideEffectsAbsolute} />
+                        </div>
+                      </div>
+
+                      <div className='col'>
+                        <h2 style={{ textDecoration: 'underline' }}>Относителни:</h2>
+                        <div className='text-content'>
+                          <NewlineText text={service.sideEffectsMinor} />
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ) : null}
+
+                {service.recovery ? (
+                  <>
+                    <div className='underline'></div>
+                    <h3>Възстановяване</h3>
+                    <div className='underline'></div>
+                    <div className='text-content'>
+                      <NewlineText text={service.recovery} />
+                    </div>
+                  </>
+                ) : null}
+
+                {service.last ? (
+                  <>
+                    <div className='underline'></div>
+                    <h3>Последваща Грижа</h3>
+                    <div className='underline'></div>
+                    <div className='text-content'>
+                      <NewlineText text={service.care} />
+                    </div>
+                  </>
+                ) : null}
+
+                {service.last ? (
+                  <div>
+                    <br></br>
+                    <div className='underline'></div>
+                    <div style={{ textAlign: 'justify' }}>{service.last}</div>
+                  </div>
+                ) : null}
               </div>
             </div>
           </div>
