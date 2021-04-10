9e331796035525acb00c59daf13e764f37d64e3b
smdnikolov <smdnikolov@gmail.com>
2021-04-10 21:26:25 +0300

    changed map

--- src/components/contacts/index.js
+++ src/components/contacts/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import map from '../../utils/map.jpg';
+import Map from './map';
 
 const Contacts = () => {
   return (
@@ -7,20 +8,18 @@ const Contacts = () => {
       <h1 className='page-header'>Контакти</h1>
       <h3>Телефон: </h3>
       <br />
-      <span>+359879997600</span>
-      <hr className='style-two' />
+      <span>+359 879 99 76 00</span>
+      <div className='underline'></div>
       <h3>Електронна поща: </h3>
       <br /> <span>elenayordanova0440@gmail.com</span>
-      <hr className='style-two' />
+      <div className='underline'></div>
       <h3>Работно време: </h3>
       <br />
       <span> Всеки ден от 9:00 до 19:00</span>
-      <hr className='style-two' />
+      <div className='underline'></div>
       <h3>Адрес на салона: </h3>
       <br />
-      <a target='_blank' rel='noopener noreferrer' href='https://goo.gl/maps/vxEXZHnkJwNuQQaF7'>
-        <img className='map' src={map} alt='' />
-      </a>
+      <Map />
     </div>
   );
 };
---
+++ src/components/contacts/map.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup, ZoomControl } from 'react-leaflet';
+
+const Map = () => {
+  return (
+    <MapContainer center={[42.722453, 23.258676]} zoom={16} scrollWheelZoom={true}>
+      <ZoomControl position='topright'></ZoomControl>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      <Marker position={[42.722453, 23.258676]}>
+        <Popup>
+          <a target='_blank' rel='noopener noreferrer' href='https://goo.gl/maps/vxEXZHnkJwNuQQaF7'>
+            Навигирай
+          </a>
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
